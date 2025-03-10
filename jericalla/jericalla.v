e5f8a5311782d408054018dea31899a9e0524624
jeedrubio <73206532+jeedrubio@users.noreply.github.com>
2025-03-10 10:24:59 -0600

    Update jericalla.v

--- jericalla/jericalla.v
+++ jericalla/jericalla.v
@@ -7,6 +7,10 @@ module jericalla(
 	rom rom_ins(.address1(instr[11:8]), .address2(instr[7:4]), .data_out1(a), .data_out2(b));
 	alu alu_ins(.sel(instr[15:12]), .a(a), .b(b), .r(r));
 	ram ram_ins(.w_en(instr[16]), .address(instr[3:0]), .data_input(r));
-	
+
+	initial begin 
+		$readmemb("data.txt", rom_inst.mem);
+	end
+
 	assign out = r;
 endmodule
