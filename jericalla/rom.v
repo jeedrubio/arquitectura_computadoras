2539a77a202c1ccbb2063589fd30d8cdaac9c17c
jeedrubio <73206532+jeedrubio@users.noreply.github.com>
2025-03-10 10:09:34 -0600

    Update rom.v

--- jericalla/rom.v
+++ jericalla/rom.v
@@ -1,10 +1,11 @@
 module rom(
-	input [3:0] address,
-	output reg [31:0] data_output
+	input [3:0] address1, address2
+	output reg [31:0] data_out1, data_out2
 );
 	reg [31:0] mem [0:15];
 	
 	always @* begin
-		data_output = mem[address];
+		data_out1 = mem[address1];
+		data_out2 = mem[address2];
 	end
 endmodule
