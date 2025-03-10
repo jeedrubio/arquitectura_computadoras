14a5d1568b12137f06bf835650c628a7411dd3ee
jeedrubio <73206532+jeedrubio@users.noreply.github.com>
2025-03-10 10:04:16 -0600

    Update ram.v

--- jericalla/ram.v
+++ jericalla/ram.v
@@ -11,26 +11,3 @@ module ram(
 		data_output = mem[address];
 	end
 endmodule
-
-`timescale 1ns/1ns
-module ram_tb();
-	reg w_en;
-	reg [6:0] address;
-	reg [7:0] data_input;
-	wire [7:0] data_output;
-	
-	ram inst(.w_en(w_en), .address(address), .data_input(data_input), .data_output(data_output));
-	initial begin 
-		$readmemb("data", inst.mem);
-	end
-	
-	initial begin
-		w_en = 1;
-		address = 12;
-		data_input = 69;
-		#100;
-		w_en = 0;
-		address = 12;
-		#100;
-	end
-endmodule
